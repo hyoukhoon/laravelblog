8cb570516b7b8ef16e04209a663648c00db70a72
escritos <escritoyane@gmail.com>
2024-05-24 13:37:22 +0900

    board

--- app/Http/Controllers/KboardController.php
+++ app/Http/Controllers/KboardController.php
@@ -17,7 +17,7 @@ class KboardController extends Controller
     public function show($num)
     {
         $boards = Kboard::findOrFail($num);
-        $boards->contents=urldecode($boards->contents);
+        $boards->contents=htmlspecialchars_decode($boards->contents);
         return view('boards.view', compact('boards'));
     }
 
