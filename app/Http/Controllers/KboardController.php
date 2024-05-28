ef47a6885e75f206ba11c1ff662ef9c07d7c2238
escritos <escritoyane@gmail.com>
2024-05-28 13:13:06 +0900

    board

--- app/Http/Controllers/KboardController.php
+++ app/Http/Controllers/KboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Kboard;
 use Illuminate\Http\Request;
+use Log;
 
 class KboardController extends Controller
 {
@@ -10,7 +11,8 @@ class KboardController extends Controller
         //Qna::orderBy('regdate', 'desc')->get();
         //$boards = Kboard::latest('reg_date')->paginate(10);
         $boards = Kboard::orderBy('num','desc')->paginate(20);
-        $boards->reg_date = disptime($boards->reg_date);
+        error_log ('['.__FILE__.']['.__FUNCTION__.']['.__LINE__.']['.date("YmdHis").']'.print_r($boards,true)."\n", 3, "/var/www/chukppa/board/upImages/data/L_".date("Ymd").'.log');
+        //$boards->reg_date = disptime($boards->reg_date);
         //return view('boards.index', compact('boards'));
         return view('boards.index', compact('boards'))->with('i', (request()->input('page', 1) - 1) * 20);
     }
