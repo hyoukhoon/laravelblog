e086607d5a43f975e711cad42f52c24a745e9261
escritos <escritoyane@gmail.com>
2024-05-29 15:12:44 +0900

    board

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -90,9 +90,10 @@
 
 <main class="form-signin w-100 m-auto">
 <form>
+<div style="text-align:center;">
   <img class="mb-4" src="/images/bootstrap-logo.svg" alt="" width="72" height="57">
   <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
+</div>
   <div class="form-floating">
     <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
     <label for="floatingInput">Email address</label>
