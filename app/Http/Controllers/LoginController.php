96e7fa319ae751a797954cdd4a428bb5b66d3643
escritos <escritoyane@gmail.com>
2024-05-30 16:53:00 +0900

    login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -22,7 +22,7 @@ class LoginController extends Controller
             'passwd'        =>   $passwd
         );
         $loginInfo = $request -> only(['email', 'passwd']);
-        //error_log ('['.__FILE__.']['.__FUNCTION__.']['.__LINE__.']['.date("YmdHis").']'.print_r($loginInfo,true)."\n", 3, "/var/www/chukppa/board/upImages/data/L_".date("Ymd").'.log');
+        error_log ('['.__FILE__.']['.__FUNCTION__.']['.__LINE__.']['.date("YmdHis").']'.print_r($form_data,true)."\n", 3, "/var/www/chukppa/board/upImages/data/L_".date("Ymd").'.log');
         if(auth() -> attempt($form_data)){
             return redirect() -> route('boards.index');
         } else{
