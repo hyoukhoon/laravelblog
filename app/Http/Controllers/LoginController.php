a24d7d07bbbe8efd175221ca9c893803ddc3eec1
escritos <escritoyane@gmail.com>
2024-05-30 17:17:23 +0900

    login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Member;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Log;
 
@@ -22,8 +23,8 @@ class LoginController extends Controller
             'passwd'        =>   $passwd
         );
         $loginInfo = $request -> only(['email', 'passwd']);
-        error_log ('['.__FILE__.']['.__FUNCTION__.']['.__LINE__.']['.date("YmdHis").'] pass => '. $request->passwd." / ".$passwd."\n", 3, "/var/www/chukppa/board/upImages/data/L_".date("Ymd").'.log');
-        if(auth() -> attempt($loginInfo)){
+        //error_log ('['.__FILE__.']['.__FUNCTION__.']['.__LINE__.']['.date("YmdHis").'] pass => '. $request->passwd." / ".$passwd."\n", 3, "/var/www/chukppa/board/upImages/data/L_".date("Ymd").'.log');
+        if (Auth::guard('web')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
             return redirect() -> route('boards.index');
         } else{
             return redirect() -> route('auth.login');
