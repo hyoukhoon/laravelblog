6aec2e590ece0fef517fa062a4444befb2851c19
escritos <escritoyane@gmail.com>
2024-06-03 13:53:32 +0900

    board

--- resources/views/boards/index.blade.php
+++ resources/views/boards/index.blade.php
@@ -45,8 +45,8 @@
                 <td>{{$board->name}}</td>
                 <td><a href="{{ route('boards.show', $board->num) }}">{{$board->subject}}</a></td>
                 <td>{{$board->cnt}}</td>
-                <td>{{$board->reg_date->format("Y-m-d")}}</td>
-                {{-- <td>{{ disptime($board->reg_date) }}</td> --}}
+                {{-- <td>{{$board->reg_date->format("Y-m-d")}}</td> --}}
+                <td>{{ disptime($board->reg_date) }}</td>
             </tr>
         @endforeach
         </tbody>
