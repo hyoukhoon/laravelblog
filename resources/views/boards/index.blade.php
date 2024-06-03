2ebc7776ac68c2e94e490da333212d24aac7d722
escritos <escritoyane@gmail.com>
2024-06-03 13:01:01 +0900

    board

--- resources/views/boards/index.blade.php
+++ resources/views/boards/index.blade.php
@@ -47,7 +47,7 @@
                 <td>{{$board->cnt}}</td>
                 {{-- <td>{{$board->reg_date->format("Y-m-d")}}</td> --}}
                 {{-- <td>{{ disptime($board->reg_date) }}</td> --}}
-                <td>{{ echo time() - strtotime($board->reg_date); }}</td>
+                <td>{{ strtotime($board->reg_date); }}</td>
             </tr>
         @endforeach
         </tbody>
