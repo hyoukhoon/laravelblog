806bde318aa7bc7b0116c54a68d38dcd0c600ab5
escritos <escritoyane@gmail.com>
2024-06-04 14:17:39 +0900

    board

--- resources/views/boards/write.blade.php
+++ resources/views/boards/write.blade.php
@@ -18,6 +18,13 @@
             <textarea class="form-control" name="content" id="content" rows="10"></textarea>
        </div>
       </div>
+      <div id="attach_site">
+          <div class="row row-cols-1 row-cols-md-6 g-4" id="attachFiles" style="margin-left:0px;">
+          </div>
+     </div>
+     <div class="mb-3">
+          <input type="file" name="afile" id="afile" accept="image/*" multiple class="form-control" aria-label="Large file input example">
+      </div>
       <br />
       <br />
       <br />
@@ -27,6 +34,96 @@
       </div>
      </form>
 <script>
+
+$("#afile").change(function(){
+	var formData = new FormData();
+	var attcnt=$("#attcnt").val();
+	var files = $('#afile').prop('files');
+	var totcnt=parseInt(attcnt)+parseInt(files.length)
+
+	if(totcnt>10){
+		alert('10개까지만 등록할 수 있습니다.');
+		return false;
+	}
+
+	for(var i=0; i < files.length; i++) {
+		attachFile(files[i]);
+	}
+});   
+
+function attachFile(file) {
+    var formData = new FormData();
+	var num=$("#num").val();
+    formData.append("file", file);
+	formData.append("uptype", "attach");
+	formData.append("num", num);
+    $.ajax({
+        url: '/board/saveImage.php',
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+		dataType : 'json' ,
+        type: 'POST',
+        success: function (return_data) {
+//			console.log(JSON.stringify(return_data));
+			if(return_data.result=='image'){
+				alert('용량이 너무 크거나 이미지 파일이 아닙니다.');
+				return;
+			}else if(return_data.result=='gif'){
+				alert(return_data.msg);
+				return;
+			}else if(return_data.result=='fail'){
+				alert(return_data.msg);
+				return false;
+			}else{
+                //var img="<img src='"+data+"' width='50'><br>";
+				var html = "<div id='"+return_data.fn+"' class='card h-100' style='width:120px;margin-right: 10px;margin-bottom: 10px;'><img src='"+return_data.savename+"' style='width:100px;'><div class='card-body'><button type='button' class='btn btn-warning' onclick=\"file_del('"+return_data.filename+"')\">삭제</button></div></div>";
+                $("#attachFiles").append(html);
+				
+                //$summernote.summernote('insertImage', data, function //($image) {
+				//	$image.attr('src', data);
+				//	$image.attr('class', 'childImg');
+				//});
+				var rcnt=parseInt(attcnt)+1;
+				$("#attcnt").val(rcnt);
+				var attachFile=$("#attachFile").val();
+				if(attachFile){
+					attachFile=attachFile+",";
+				}
+				$("#attachFile").val(attachFile+return_data.filename);
+			}
+        }
+		, beforeSend: function () {
+              var width = 0;
+              var height = 0;
+              var left = 0;
+              var top = 0;
+              width = 50;
+              height = 50;
+
+			  top = ( $(window).height() - height ) / 2 + $(window).scrollTop();
+              left = ( $(window).width() - width ) / 2 + $(window).scrollLeft();
+
+              if($("#div_ajax_load_image").length != 0) {
+                     $("#div_ajax_load_image").css({
+                            "top": top+"px",
+                            "left": left+"px"
+                     });
+                     $("#div_ajax_load_image").show();
+              }
+              else {
+                     $('body').append('<div id="div_ajax_load_image" style="position:absolute; top:' + top + 'px; left:' + left + 'px; width:' + width + 'px; height:' + height + 'px; z-index:9999;" class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>');
+              }
+
+       }
+	    , complete: function () {
+                     $("#div_ajax_load_image").hide();
+       }
+    });
+
+}
+
      function sendsubmit(){
           var subject=$("#subject").val();
           var content=$("#content").val();
