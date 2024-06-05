e92fc8ab554334697a139fb20477d66659f06c7e
escritos <escritoyane@gmail.com>
2024-06-05 13:07:47 +0900

    memo

--- resources/views/boards/view.blade.php
+++ resources/views/boards/view.blade.php
@@ -45,7 +45,9 @@
                     <table>
                         <tbody><tr class="align-middle">
                             <td rowspan="2" class="pr-2">
-                                                        <span class="material-symbols-outlined" style="font-size:40px;">record_voice_over</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5"/>
+                                  </svg>
                             </td>
                             <td class="ml">파르티<span class="material-symbols-outlined" style="vertical-align: text-bottom;">looks_one</span></td>
                         </tr>
@@ -94,7 +96,9 @@
                                     <table>
                                         <tbody><tr class="align-middle">
                                             <td rowspan="2" class="pr-2">
-                                                <span class="material-symbols-outlined" style="font-size:40px;">record_voice_over</span>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
+                                                    <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5"/>
+                                                  </svg>
                                             </td>
                                             <td class="ml">칸토나</td>
                                         </tr>
@@ -120,7 +124,9 @@
                                     <table>
                                         <tbody><tr class="align-middle">
                                             <td rowspan="2" class="pr-2">
-                                                <span class="material-symbols-outlined" style="font-size:40px;">record_voice_over</span>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
+                                                    <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5"/>
+                                                  </svg>
                                             </td>
                                             <td class="ml">칸토나</td>
                                         </tr>
