5926cc9d4a4f7ed84b29fd57ed69e2faf284c806
escritos <escritoyane@gmail.com>
2024-06-05 13:52:26 +0900

    memo

--- resources/views/boards/view.blade.php
+++ resources/views/boards/view.blade.php
@@ -67,6 +67,17 @@
                 <span class="badge bg-secondary" style="cursor:pointer;padding:10px;"><a onclick="memo_delete('30','4513')">삭제</a></span>
             </div>
         </div>
+        <div class="input-group" style="margin-top:10px;margin-bottom:10px;" id="memo_reply_area_35">
+            <div class="p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z"></path>
+                </svg>
+            </div>
+            <input type="hidden" name="memoid" id="memoid" value="35">
+            <input type="hidden" name="bid" id="bid" value="4513">
+            <textarea class="form-control" aria-label="With textarea" name="memo_35" id="memo_35" placeholder="대댓글을 입력해주세요"></textarea>
+            <button type="button" class="btn btn-secondary" style="float:right;" id="memo_submit_reply_35" onclick="memo_submit_reply()">입력</button>
+        </div>
         @endforeach
     </div>
 
@@ -111,7 +122,10 @@
                         <table>
                             <tbody><tr class="align-middle">
                                 <td rowspan="2" class="pr-2">
-                                    대댓글:
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-square-text" viewBox="0 0 16 16">
+                                        <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                                        <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6m0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5"/>
+                                      </svg>
                                 </td>
                                 <td class="ml">칸토나</td>
                             </tr>
