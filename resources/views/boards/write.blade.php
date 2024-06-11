fc1d3ba3a27d3dab21e14998a4c6c6920fefac24
escritos <escritoyane@gmail.com>
2024-06-11 16:39:41 +0900

    summernote

--- resources/views/boards/write.blade.php
+++ resources/views/boards/write.blade.php
@@ -41,7 +41,7 @@
 <script>
 
 $(document).ready(function() {
-     $('#summernote').summernote({
+     var $summernote = $('#summernote').summernote({
           placeholder: '내용을 입력하세요.',
           tabsize: 2,
           height: 400,
