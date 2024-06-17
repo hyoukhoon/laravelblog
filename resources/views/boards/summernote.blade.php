0d488d5fb80f5e5c5bc39fbefb41e3e56e0cfd53
escritos <escritoyane@gmail.com>
2024-06-17 10:57:26 +0900

    board

--- resources/views/boards/summernote.blade.php
+++ resources/views/boards/summernote.blade.php
@@ -4,7 +4,7 @@
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 
-<div id="summernote"></div>
+<div id="summernote">{!! $boards->content !!}</div>
 
 <script>
 $(document).ready(function() {
