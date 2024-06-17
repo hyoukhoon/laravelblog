262fbe9262138d10aebd828126835f71c8fc59aa
escritos <escritoyane@gmail.com>
2024-06-17 14:56:09 +0900

    board

--- resources/views/boards/edit.blade.php
+++ resources/views/boards/edit.blade.php
@@ -4,6 +4,9 @@
      <form method="post" action="{{ route('boards.update', $boards->num) }}" enctype="multipart/form-data">
         @csrf
         @method('post')
+      <input type="hidden" name="attcnt" id="attcnt" value="0">
+      <input type="hidden" name="imgUrl" id="imgUrl" value="">
+      <input type="hidden" name="attachFile" id="attachFile" value="">
       <div class="form-group">
        <label class="col-md-4 text-right">제목</label>
        <div class="col-md-8">
@@ -18,7 +21,19 @@
       </div>
       <br />
       <br />
-      <br />
+      <div id="attach_site" class="col-md-8">
+         <div class="row row-cols-1 row-cols-md-6 g-4" id="attachFiles" style="margin-left:0px;">
+            {{
+               $attachfiles = explode(",",$boards->attachfile);
+            }}
+            @foreach ($attachfiles as $af)
+               <div id='af_{{ $af }}' class='card h-100' style='width:120px;margin-right: 10px;margin-bottom: 10px;'><img src='/images/{{ $af }}' width='100' /><div class='card-body'><button type='button' class='btn btn-warning' onclick='deletefile("{{ $af }}")'>삭제</button></div></div>
+            @endforeach
+         </div>
+      </div>
+      <div class="col-md-8">
+         <input type="file" name="afile" id="afile" accept="image/*" multiple class="form-control" aria-label="Large file input example">
+    </div>
       <div class="col-md-8 form-group text-center">
        <input type="submit" name="edit" class="btn btn-primary input-lg" value="수정" />
       </div>
