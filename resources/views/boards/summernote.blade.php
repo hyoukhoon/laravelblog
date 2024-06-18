974d974e4e0ad44dfce5d3970967d12cc7721fca
escritos <escritoyane@gmail.com>
2024-06-18 11:20:17 +0900

    board

--- resources/views/boards/summernote.blade.php
+++ resources/views/boards/summernote.blade.php
@@ -4,7 +4,7 @@
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 
-@if($boards->content)
+@if($boards)
     <div id="summernote">{!! $boards->content !!}</div>
 @else
     <div id="summernote"></div>
