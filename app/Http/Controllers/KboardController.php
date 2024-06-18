b7b5d25e1cb1c92639f889bd2153993b73fb856a
escritos <escritoyane@gmail.com>
2024-06-18 11:15:36 +0900

    board

--- app/Http/Controllers/KboardController.php
+++ app/Http/Controllers/KboardController.php
@@ -21,7 +21,7 @@ class KboardController extends Controller
     public function write()
     {
         if(auth()->check()){
-            $boards = array();
+            $boards = new stdClass();
             return view('boards.write', compact('boards'));
         }else{
             return redirect()->back()->with('로그인 하십시오.');
