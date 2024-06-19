a2c8e504c104a17f4e394ce0e3a54a6d64dc6472
escritos <escritoyane@gmail.com>
2024-06-19 15:17:17 +0900

    board

--- resources/views/boards/index.blade.php
+++ resources/views/boards/index.blade.php
@@ -22,8 +22,6 @@
     <h2 class="mt-4 mb-3">게시판 목록</h2>
     <a href="{{route('boards.write')}}"><button class="text-xl">등록</button></a>
         <form method="get" action="/boards/search">
-            @csrf
-            @method('get')
             <input type="text" name="search" value="">
             <input type="submit" value="검색">
         </form>
