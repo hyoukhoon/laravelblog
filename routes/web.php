b2e90a5215f04b4747c70217f7f706a021865d00
escritos <escritoyane@gmail.com>
2024-06-20 16:48:52 +0900

    board

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,6 @@ Route::post('/products/store', [ProductController::class, 'store'])->name('produ
 Route::get('/qna', [QnaController::class, 'index'])->name('qna.index');
 
 Route::get('/boards', [KboardController::class, 'index'])->name('boards.index');
-//Route::get('/boards/search/', [KboardController::class, 'search'])->name('boards.search');
 Route::get('/boards/search/', [KboardController::class, 'search'])->name('boards.search');
 Route::get('/boards/write', [KboardController::class, 'write'])->name('boards.write');
 Route::get('/boards/summernote', [KboardController::class, 'summernote'])->name('boards.summernote');
