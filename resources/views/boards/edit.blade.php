21569e93d021e87f4e4abdaa02472e830421835b
escritos <escritoyane@gmail.com>
2024-06-20 13:26:46 +0900

    board

--- resources/views/boards/edit.blade.php
+++ resources/views/boards/edit.blade.php
@@ -23,12 +23,10 @@
       <br />
       <div id="attach_site" class="col-md-8">
          <div class="row row-cols-1 row-cols-md-6 g-4" id="attachFiles" style="margin-left:0px;">
-            <?php
-               //$attachfiles = explode(",",$boards->attachfile);
-               //echo $boards->attachfile;
-            ?>
             @foreach ($boards->attfiles as $af)
-               <div id="af_{{ substr($af,0,10) }}" class='card h-100' style='width:120px;margin-right: 10px;margin-bottom: 10px;'><img src="/images/{{ $af }}" width='100' /><div class='card-body'><button type='button' class='btn btn-warning' onclick="deletefile('{{ $af }}')">삭제</button></div></div>
+               @if($af)
+                  <div id="af_{{ substr($af,0,10) }}" class='card h-100' style='width:120px;margin-right: 10px;margin-bottom: 10px;'><img src="/images/{{ $af }}" width='100' /><div class='card-body'><button type='button' class='btn btn-warning' onclick="deletefile('{{ $af }}')">삭제</button></div></div>
+               @endif
             @endforeach
          </div>
       </div>
