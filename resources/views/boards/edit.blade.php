4105ae9c36757f15cc7f1ed202e5c7b5e681ea98
escritos <escritoyane@gmail.com>
2024-07-05 13:12:45 +0900

    board

--- resources/views/boards/edit.blade.php
+++ resources/views/boards/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('boards.layout')
 @section('content')
 <br />
-     <form method="post" action="{{ route('boards.update', $boards->num) }}" enctype="multipart/form-data">
+     <form method="post" action="{{ route('boards.update') }}" enctype="multipart/form-data">
         @csrf
         @method('post')
       <input type="hidden" name="attcnt" id="attcnt" value="0">
