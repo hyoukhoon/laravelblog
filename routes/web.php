3ef910af4ac6e6f06831087e1e2100bf12131a6f
escritos <escritoyane@gmail.com>
2024-07-05 14:23:52 +0900

    board

--- routes/web.php
+++ routes/web.php
@@ -21,19 +21,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/crud', [CrudsController::class, 'index'])->name('crud.index');
-Route::get('/show/{id}', [CrudsController::class, 'show'])->name('crud.show');
-Route::get('/edit/{id}', [CrudsController::class, 'edit'])->name('crud.edit');
-Route::delete('/destroy/{id}', [CrudsController::class, 'destroy'])->name('crud.destroy');
-Route::post('/store', [CrudsController::class, 'store'])->name('crud.store');
-Route::post('/update/{id}', [CrudsController::class, 'update'])->name('crud.update');
-Route::get('/create', [CrudsController::class, 'create']);
-//Route::get('/crud', 'App\Http\Controllers\CrudsController@index');
-
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
-
 Route::get('/qna', [QnaController::class, 'index'])->name('qna.index');
 
 Route::get('/boards', [KboardController::class, 'index'])->name('boards.index');
