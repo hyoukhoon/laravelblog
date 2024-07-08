8a5a04b7e7c050031ab2fcd001c4fbb655af2d95
escritos <escritoyane@gmail.com>
2024-07-08 15:29:53 +0900

    board

--- app/Http/Controllers/KboardController.php
+++ app/Http/Controllers/KboardController.php
@@ -250,7 +250,7 @@ class KboardController extends Controller
         if(auth()->check()){
             $rpts = report::where('bid', $request->bid)
                         ->where('userid',Auth::user()->email)->get();
-            if($rpts){
+            if(count($rpts)>0){
                 return response()->json(array('msg'=> "이미 신고하신 게시물입니다.", 'result'=>false), 200);
             }else{
                 $rs = $insert_data->save(); // 여기서 $rs는 true만 리턴
