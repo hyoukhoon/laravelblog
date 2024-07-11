da888391e3724595f841f94437784d9b6b1ab6d5
escritos <escritoyane@gmail.com>
2024-07-11 10:27:10 +0900

    signup

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -39,6 +39,24 @@ class LoginController extends Controller
         }
     }
 
+    public function signupok(Request $request){
+        $passwd = $request->password;
+        $passwd = hash('sha512',$passwd);
+        $form_data = array(
+            'email' => $request->email,
+            'passwd' => $passwd,
+            'name' => $request->name
+        );
+
+        $rs=Member::create($form_data);
+        
+        if($rs){
+            return response()->json(array('msg'=> "가입해 주셔서 감사합니다.", 'result'=>true), 200);
+        }else{
+            return response()->json(array('msg'=> "실패했습니다. 관리자에게 문의해주세요.", 'result'=>false), 200);
+        }
+    }
+
     public function login(Request $request){
         $email = $request->email;
         $passwd = $request->passwd;
