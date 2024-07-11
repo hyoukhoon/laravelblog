168072bd0c5862e26202c9e64c3ba1d710d79866
escritos <escritoyane@gmail.com>
2024-07-11 16:40:58 +0900

    board

--- app/Http/Controllers/KboardController.php
+++ app/Http/Controllers/KboardController.php
@@ -22,12 +22,6 @@ class KboardController extends Controller
         return view('boards.index', compact('boards'))->with('i', (request()->input('page', 1) - 1) * 20);
     }
 
-    // public function search($search){
-    //     $param = "%".$search."%";
-    //     $boards = Kboard::where('subject', 'LIKE', $param)->orderBy('num', 'desc')->paginate(20);
-    //     return view('boards.index', compact('boards','search'))->with('i', (request()->input('page', 1) - 1) * 20);
-    // }
-
     public function write()
     {
         if(auth()->check()){
